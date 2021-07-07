45138419b28afe41a88cd8242affe7c1afe79e88
guale <gualeshadow@gmail.com>
2021-07-07 11:02:25 -0500

    finish class

--- Day 3.2/assets/functions.js
+++ Day 3.2/assets/functions.js
@@ -1,1 +1,46 @@
-console.log("Script is loaded")
+// console.log("Script is loaded")
+
+// let a = 1;
+// let b = 2;
+
+// function add() {
+// 	return a + b;
+// }
+
+// console.log(add());
+
+// function add2(a, b) {
+// 	return a + b;
+// }
+
+// console.log(add2(7, 9));
+
+// function addAll() {
+// 	let sum = 0;
+// 	for (let i = 0; i < arguments.length; i++) {
+// 		sum += arguments[i];
+// 	}
+// 	return sum;
+// }
+
+// console.log(addAll(2, 3, 4, 5, 6, 7, 8));
+
+// function addAll2(...rest) {
+// 	let sum = 0;
+// 	for (let i = 0; i < rest.length; i++) {
+// 		sum += rest[i];
+// 	}
+// 	return sum;
+// }
+
+let billAmount = 420.69;
+
+function gratuity() {
+	return parseFloat((billAmount*.2).toFixed(2));
+}
+
+function totalWithGrat() {
+	return billAmount + gratuity();
+}
+
+console.log(totalWithGrat());
