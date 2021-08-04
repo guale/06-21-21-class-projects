75741c9f7b20ee5ca6d76334dd5b0bfa0c247247
guale <gualeshadow@gmail.com>
2021-08-04 15:10:45 -0500

    add basic calculator functions

--- Java Calculator/src/Calculator.java
+++ Java Calculator/src/Calculator.java
@@ -1,3 +1,66 @@
 public class Calculator {
-	
+
+
+	public static double add(double num1, double num2) {
+		return num1 + num2;
+	}
+
+	public static double subtract(double num1, double num2) {
+		return num1 - num2;
+	}
+
+	public static double multiply(double num1, double num2) {
+		return num1 * num2;
+	}
+
+	public static double divide(double num1, double num2) {
+		return num1 / num2;
+	}
+
+	public static double power(double num1, int num2) {
+		double powered = 1;
+		for (int i = 0; i < num2; i++) {
+			powered *= num1;
+		}
+		return powered;
+	}
+
+	public static double squareRoot(double num) {
+		return Math.sqrt(num);
+	}
+
+	public static double sin(double num) {
+		return Math.sin(num);
+	}
+
+	public static double sinDegrees(double num) {
+		num = Math.toDegrees(num);
+		return Math.sin(num);
+	}
+
+	public static double cos(double num) {
+		return Math.cos(num);
+	}
+
+	public static double cosDegrees(double num) {
+		num = Math.toDegrees(num);
+		return Math.cos(num);
+	}
+
+	public static double tan(double num) {
+		return Math.tan(num);
+	}
+
+	public static double tanDegrees(double num) {
+		num = Math.toDegrees(num);
+		return Math.tan(num);
+	}
+
+	public static long factorial(int num) {
+		long factorialed = num;
+		for (int i = 0; i < num; i++) {
+			factorialed *= (--num);
+		}
+		return factorialed;
+	}
 }
--- Java Calculator/src/Main.java
+++ Java Calculator/src/Main.java
@@ -1,5 +1,67 @@
-public class Main {
-	public static void main(String[] args){
+import java.util.Scanner;
 
+public class Main {
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		boolean cont = false;
+
+		do {
+			System.out.println("Please select an operation: \n" +
+					"Add\t\tSubtract\tMultiply\tDivide\tPower \n" +
+					"Sqrt\tSin\t\t\tCos\t\t\tTan\t\tFactorial");
+			String response = (scan.nextLine()).toLowerCase();
+
+			switch (response) {
+				case "add"->{
+					System.out.println("Please enter two numbers separate by a space.");
+					final double num1 = scan.nextDouble();
+					final double num2 = scan.nextDouble();
+					double result = Calculator.add(num1,num2);
+					System.out.printf("%s plus %s equals %s%n",num1,num2,result);
+				}
+				case "subtract"->{
+					System.out.println("Please enter two numbers separate by a space.");
+					final double num1 = scan.nextDouble();
+					final double num2 = scan.nextDouble();
+					double result = Calculator.subtract(num1,num2);
+					System.out.printf("%s minus %s equals %s%n",num1,num2,result);
+				}
+				case "multiply"->{
+					System.out.println("Please enter two numbers separate by a space.");
+					final double num1 = scan.nextDouble();
+					final double num2 = scan.nextDouble();
+					double result = Calculator.multiply(num1,num2);
+					System.out.printf("%s times %s equals %s%n",num1,num2,result);
+				}
+				case "divide"->{
+					System.out.println("Please enter two numbers separate by a space.");
+					final double num1 = scan.nextDouble();
+					final double num2 = scan.nextDouble();
+					double result = Calculator.divide(num1,num2);
+					System.out.printf("%s divided by %s equals %s%n",num1,num2,result);
+				}
+				case "power"->{
+					System.out.println("Please enter an integer.");
+					int num1 = scan.nextInt();
+					System.out.println("To be raised to the power of a second integer.");
+					int num2 = scan.nextInt();
+					System.out.println(Calculator.power(num1,num2));
+				}
+			}
+
+			System.out.println("Would you like to continue? (y/N");
+			scan.nextLine();
+			response = (scan.nextLine()).toLowerCase();
+
+			switch (response){
+				case "y","yes" -> cont = true;
+				default -> cont = false;
+			}
+
+
+		} while (cont);
+		System.out.println("Thank you, come again.");
 	}
 }
+
