239429edb98210f9d3a33de245cca549d2e2f2c5
guale <gualeshadow@gmail.com>
2021-08-24 12:15:02 -0500

    complete edit and delete functions

--- addressbook/src/main/java/com/tts/addressbook/controller/AddressBookController.java
+++ addressbook/src/main/java/com/tts/addressbook/controller/AddressBookController.java
@@ -3,10 +3,12 @@ package com.tts.addressbook.controller;
 import com.tts.addressbook.module.Address;
 import com.tts.addressbook.repository.AddressBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class AddressBookController {
@@ -16,11 +18,12 @@ public class AddressBookController {
 
 	@GetMapping("/")
 	public String index(Address address, Model model) {
-		model.addAttribute("entries", addressBookRepository.findAll());
+		List<Address> addresses = addressBookRepository.findAll(Sort.by(Sort.Direction.ASC, "firstName"));
+		model.addAttribute("entries", addresses);
 		return "address/index";
 	}
 
-	@PostMapping("/add")
+	@PostMapping("/addresses")
 	public String addNewAddress(Address address, Model model) {
 		if (!address.getMiddleName().equals(null)) {
 			address.setFullName(address.getFirstName() + " " + address.getMiddleName() + " " + address.getLastName());
@@ -39,5 +42,50 @@ public class AddressBookController {
 		return "address/result";
 	}
 
+	@GetMapping("/addresses/new")
+	public String newAddress(Address address){
+		return "address/new";
+	}
 
+	@GetMapping("/addresses/{id}")
+	public String editPostById(@PathVariable Long id, Address address, Model model) {
+		model.addAttribute("address", addressBookRepository.findById(id).orElseThrow());
+		return("address/edit");
+	}
+
+	@PostMapping("/addresses/update/{id}")
+	public String updateExistingAddress(@PathVariable Long id, Address address, Model model) {
+		var workingAddress = addressBookRepository.findById(id).orElseThrow();
+		workingAddress.setFirstName(address.getFirstName());
+		workingAddress.setMiddleName(address.getMiddleName());
+		workingAddress.setLastName(address.getLastName());
+		workingAddress.setPhoneNumber(address.getPhoneNumber());
+		workingAddress.setEmailAddress(address.getEmailAddress());
+		workingAddress.setComment(address.getComment());
+		if (!address.getMiddleName().equals(null)) {
+			address.setFullName(address.getFirstName() + " " + address.getMiddleName() + " " + address.getLastName());
+		} else {
+			address.setFullName(address.getFirstName() + " " + address.getLastName());
+		}
+		addressBookRepository.save(address);
+		model.addAttribute("address", workingAddress);
+		return "address/result";
+	}
+
+	@RequestMapping("/addresses/delete/{id}")
+	public String deleteAddress(@PathVariable Long id, Address address){
+		addressBookRepository.deleteById(id);
+		return "address/delete";
+	}
+
+	@GetMapping("/addresses/clear")
+	public String clear(){
+		return "address/clear";
+	}
+
+	@RequestMapping("/addresses/clearAll")
+	public String clearAll(){
+		addressBookRepository.deleteAll();
+		return "address/clearConfirm";
+	}
 }
--- addressbook/src/main/java/com/tts/addressbook/repository/AddressBookRepository.java
+++ addressbook/src/main/java/com/tts/addressbook/repository/AddressBookRepository.java
@@ -1,7 +1,9 @@
 package com.tts.addressbook.repository;
 
 import com.tts.addressbook.module.Address;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressBookRepository extends CrudRepository<Address, Long> {
+import java.util.List;
+
+public interface AddressBookRepository extends JpaRepository<Address, Long> {
 }
