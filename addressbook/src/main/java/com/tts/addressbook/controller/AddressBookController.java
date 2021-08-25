fbcef21b84ffc383d09979da25d75375fe848a04
guale <gualeshadow@gmail.com>
2021-08-25 12:13:12 -0500

    fix bug in edit

--- addressbook/src/main/java/com/tts/addressbook/controller/AddressBookController.java
+++ addressbook/src/main/java/com/tts/addressbook/controller/AddressBookController.java
@@ -70,6 +70,8 @@ public class AddressBookController {
 		address.setPhoneNumber(convertToNumbers(address.getPhoneNumber()));
 
 		addressBookRepository.save(address);
+		
+		address.setPhoneNumber(formatPhoneNumber(address.getPhoneNumber()));
 
 		return "address/result";
 	}
